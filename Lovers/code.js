87f6464b0d785a9fe4acdae33c1906e24be4d3db
Mohacking <jhanersongutierrez@gmail.com>
2023-06-19 17:05:07 -0500

    Mohacking-01

--- Lovers/code.js
+++
@@ -1,7 +0,0 @@
-            // Seleccionar el elemento con la clase "navbar-brand"
-            const navbarBrand = document.querySelector('.navbar-brand');
-
-            // Esperar 5 segundos y ocultar el nombre de la navbar
-            setTimeout(() => {
-                navbarBrand.style.display = 'none';
-            }, 5000);

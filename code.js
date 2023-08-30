e894205f6a3a0e9dff8828f99f5b78a26f94eb1d
Mohacking <96668230+jhanerson@users.noreply.github.com>
2023-08-29 21:23:52 -0500

    V-2-001

--- code.js
+++ code.js
@@ -2,3 +2,16 @@ window.onload = function () {
 
     $('#onload').fadeOut();
 }
+
+
+window.addEventListener('load', function () {
+    const carouselItems = document.querySelectorAll('.carousel-item');
+    const randomIndex = Math.floor(Math.random() * carouselItems.length);
+    carouselItems.forEach((item, index) => {
+        if (index === randomIndex) {
+            item.classList.add('active');
+        } else {
+            item.classList.remove('active');
+        }
+    });
+});

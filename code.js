795917c6ddf1d25f63ca4d47ef0147342a4e759d
Mohacking <96668230+jhanerson@users.noreply.github.com>
2023-09-01 16:18:43 -0500

    V-1

--- code.js
+++ code.js
@@ -15,3 +15,7 @@ window.addEventListener('load', function () {
         }
     });
 });
+
+
+
+document.querySelector("#__next > main > div.col-span-4.flex.items-center.justify-center.overflow-hidden.rounded-2xl.bg-pink-200.dark\\:border-pink-500.dark\\:bg-pink-500\\/20.dark\\:shadow-none.dark\\:backdrop-blur-2xl.md\\:col-span-4.md\\:h-52 > div")
